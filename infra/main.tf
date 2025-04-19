dd7260e398740f13a5619856d0a1f24849f03030
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-19 22:14:28 +0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,27 +9,30 @@ terraform {
 
 provider "azurerm" {
   features {}
+
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
 
-# Use existing resource group
+# Get existing Resource Group
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-# Dynamically select resource group name and location
-locals {
-  rg_name     = data.azurerm_resource_group.rg.name
-  rg_location = data.azurerm_resource_group.rg.location
-} 
+# Get existing App Service Plan
+data "azurerm_service_plan" "existing_asp" {
+  name                = var.app_service_plan_name
+  resource_group_name = var.resource_group_name
+}
+
+# Azure Linux Web App
 resource "azurerm_linux_web_app" "web_app" {
-  name                = var.app_service_name  # Should be "demo-app-faruk-dev-001"
-  location            = local.rg_location
-  resource_group_name = local.rg_name
-  service_plan_id     = azurerm_service_plan.app_service_plan.id
+  name                = var.app_service_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  service_plan_id     = data.azurerm_service_plan.existing_asp.id
 
   site_config {
     application_stack {
@@ -42,7 +45,10 @@ resource "azurerm_linux_web_app" "web_app" {
   }
 }
 
+# ------------------------------------
 # Variables
+# ------------------------------------
+
 variable "subscription_id" {
   type = string
 }
@@ -64,21 +70,10 @@ variable "resource_group_name" {
   type = string
 }
 
-variable "resource_group_location" {
-  type    = string
-  default = "East US"
-}
-
-variable "create_resource_group" {
-  type    = bool
-  default = false
-}
-
-variable "environment" {
+variable "app_service_plan_name" {
   type = string
 }
 
 variable "app_service_name" {
   type = string
 }
-
