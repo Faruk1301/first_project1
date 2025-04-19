716172e29a3755de142b07c2a4a170e8e131b478
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-19 11:13:18 +0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Terraform + Azure DevOps & cloud engineering from india and banglaadesh and hate america "
+    return "Hello from Terraform + Azure DevOps & cloud engineering from india and banglaadesh and hate america and israil "
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
