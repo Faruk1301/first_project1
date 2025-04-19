9082e9dc1043bfe7be3bda1222ee268ef3597e79
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-19 15:14:21 +0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -67,12 +67,41 @@ resource "azurerm_linux_web_app" "web_app" {
 }
 
 # Variable Declarations
-variable "subscription_id" { type = string }
-variable "client_id" { type = string }
-variable "client_secret" { type = string, sensitive = true }
-variable "tenant_id" { type = string }
-variable "resource_group_name" { type = string }
-variable "resource_group_location" { type = string, default = "East US" }
-variable "create_resource_group" { type = bool, default = false }
-variable "environment" { type = string }
-variable "app_service_name" { type = string }
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type    = string
+  default = "East US"
+}
+
+variable "create_resource_group" {
+  type    = bool
+  default = false
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "app_service_name" {
+  type = string
+}
