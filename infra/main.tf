1e4232dda31d1fea171b4f1c71ff58b81768a09e
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-20 12:32:06 +0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -60,10 +60,4 @@ variable "environment" {
   type = string
 }
 
-# ------------------------------------
-# Output (Optional)
-# ------------------------------------
 
-output "web_app_name" {
-  value = azurerm_linux_web_app.web_app.name
-}
