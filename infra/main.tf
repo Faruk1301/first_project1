c7ecf393d39de8821a6a8af7636f193b0a9633ff
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-05-10 21:05:19 +0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -40,3 +40,25 @@ resource "azurerm_app_service" "staging" {
     linux_fx_version = "PYTHON|3.9"
   }
 }
+
+# Variable declarations
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
